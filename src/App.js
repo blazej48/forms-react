b690e903814337e878eb69d2fe09555105e94c69
blazejszum <muzsbl@gmail.com>
2018-11-14 10:35:49 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
+import {UserForm} from "./UserForm";
+import {UserDetails} from "./UserDetails";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <div className="language-bar">
+                    <button className="btn" >pl</button>
+                    <button className="btn" >en</button>
+                </div>
+                <BrowserRouter>
+                    <div>
+                        <ul className="router-bar">
+                            <li>
+                                <Link to="/user-form">user form</Link>
+                            </li>
+                            <li>
+                                <Link to="/user-details">user details</Link>
+                            </li>
+                        </ul>
+                        <div className="container">
+                            <Route exact path="/user-form" component={UserForm}/>
+                            <Route path="/user-details" component={UserDetails}/>
+                        </div>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/UserDetails.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export class UserDetails extends Component {
+    render() {
+        return (
+            'user details'
+        );
+    }
+}
